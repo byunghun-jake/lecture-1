8b6cead3918a57b0b8a980f54e02142b5aa23752
byunghun-jake <figma@kakao.com>
2021-11-17 00:22:41 +0900

    1강 완료 - 이미지 최적화 (사이즈) - 텍스트 압축 - 코드 분할 (라우트 기반) -  Lighthouse, performance 탭 활용 - 번들 애널라이저 활용

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react"
 
-import './index.css'
+import "./index.css"
 
 function zeroPad(value, len) {
-  const str = '0000000000' + value.toString()
+  const str = "0000000000" + value.toString()
   return str.substring(str.length - len)
 }
 
 /* 파라미터 참고: https://unsplash.com/documentation#supported-parameters */
-function getParametersForUnsplash({width, height, quality, format}) {
+function getParametersForUnsplash({ width, height, quality, format }) {
   return `?w=${width}&h=${height}&q=${quality}&fm=${format}&fit=crop`
 }
 
@@ -17,21 +17,42 @@ function getParametersForUnsplash({width, height, quality, format}) {
  * (Markdown으로 된 문자열의 특수문자를 제거하기 위함)
  * */
 function removeSpecialCharacter(str) {
-  const removeCharacters = ['#', '_', '*', '~', '&', ';', '!', '[', ']', '`', '>', '\n', '=', '-']
-  let _str = str
-  let i = 0,
-    j = 0
+  /**
+   * 1. 필요한 문자열 만큼만 뽑아서 문자 변환한다.
+   * 2. replace 메서드를 사용한다.
+   */
+  let _str = str.substring(0, 300)
+  _str = _str.replace(/[#_*~&;![\]`>\n=-]/g, "")
 
-  for (i = 0; i < removeCharacters.length; i++) {
-    j = 0
-    while (j < _str.length) {
-      if (_str[j] === removeCharacters[i]) {
-        _str = _str.substring(0, j).concat(_str.substring(j + 1))
-        continue
-      }
-      j++
-    }
-  }
+  // const removeCharacters = [
+  //   "#",
+  //   "_",
+  //   "*",
+  //   "~",
+  //   "&",
+  //   ";",
+  //   "!",
+  //   "[",
+  //   "]",
+  //   "`",
+  //   ">",
+  //   "\n",
+  //   "=",
+  //   "-",
+  // ]
+  // let i = 0,
+  //   j = 0
+
+  // for (i = 0; i < removeCharacters.length; i++) {
+  //   j = 0
+  //   while (j < _str.length) {
+  //     if (_str[j] === removeCharacters[i]) {
+  //       _str = _str.substring(0, j).concat(_str.substring(j + 1))
+  //       continue
+  //     }
+  //     j++
+  //   }
+  // }
 
   return _str
 }
@@ -39,20 +60,33 @@ function removeSpecialCharacter(str) {
 function Article(props) {
   const createdTime = new Date(props.createdTime)
   return (
-    <div className={'Article'}>
-      <div className={'Article__summary'}>
-        <div className={'Article__summary__title'}>{props.title}</div>
-        <div className={'Article__summary__desc'}>{removeSpecialCharacter(props.content)}</div>
-        <div className={'Article__summary__etc'}>
+    <div className={"Article"}>
+      <div className={"Article__summary"}>
+        <div className={"Article__summary__title"}>{props.title}</div>
+        <div className={"Article__summary__desc"}>
+          {removeSpecialCharacter(props.content)}
+        </div>
+        <div className={"Article__summary__etc"}>
           {createdTime.getFullYear() +
-            '.' +
+            "." +
             zeroPad(createdTime.getMonth() + 1, 2) +
-            '.' +
+            "." +
             zeroPad(createdTime.getDate(), 2)}
         </div>
       </div>
-      <div className={'Article__thumbnail'}>
-        <img src={props.image + getParametersForUnsplash({width: 1200, height: 1200, quality: 80, format: 'jpg'})} alt="thumbnail" />
+      <div className={"Article__thumbnail"}>
+        <img
+          src={
+            props.image +
+            getParametersForUnsplash({
+              width: 240,
+              height: 240,
+              quality: 80,
+              format: "jpg",
+            })
+          }
+          alt="thumbnail"
+        />
       </div>
     </div>
   )
